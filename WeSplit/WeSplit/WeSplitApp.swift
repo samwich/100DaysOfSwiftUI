bc9fad0e881dd6162466ec8068a193f66d0e99b9
Sam Johnson <sam@samwich.com>
2022-11-04 20:58:31 -0700

    Day 16 WeSplit Intro

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Sam Johnson on 11/4/22.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
