93d8b661bb249437b510a7d166b9b37885660516
Sam Johnson <sam@samwich.com>
2022-11-06 13:20:24 -0800

    Begin Day 19 UnitConverter

---
+++ UnitConverter/UnitConverter/UnitConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConverterApp.swift
+//  UnitConverter
+//
+//  Created by Sam Johnson on 2022-11-06.
+//
+
+import SwiftUI
+
+@main
+struct UnitConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
