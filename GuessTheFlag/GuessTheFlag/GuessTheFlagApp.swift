7f74e6d879607abe2a5aa5b6cccbdc4d881d3e18
Sam Johnson <sam@samwich.com>
2022-11-07 14:05:14 -0800

    Begin day 20 Guess the Flag

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Sam Johnson on 2022-11-07.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
