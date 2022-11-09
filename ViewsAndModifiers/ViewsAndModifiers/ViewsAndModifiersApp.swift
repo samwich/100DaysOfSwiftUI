00919a5c4db1184bee90d84914278645a2ab73f5
Sam Johnson <sam@samwich.com>
2022-11-09 11:47:52 -0800

    Day 23 Begin Views and Modifiers

---
+++ ViewsAndModifiers/ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by Sam Johnson on 2022-11-09.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
