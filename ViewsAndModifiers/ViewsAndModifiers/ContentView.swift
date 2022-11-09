31c299968d807b91120fc7da05ec671673cc5ca7
Sam Johnson <sam@samwich.com>
2022-11-09 12:14:16 -0800

    Why does SwiftUI use "some View" for its view type?

--- ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -9,23 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Button("Hello, world!") {
-                print(type(of: self.body))
-            }
-            .frame(width: 200, height: 200)
-            .background(.red)
-            
-            Text("Hello")
-                .padding()
-                .background(.red)
-                .padding()
-                .background(.blue)
-                .padding()
-                .background(.green)
-                .padding()
-                .background(.yellow)
+        Button("Hello, world!") {
+            print(type(of: self.body))
         }
+        .frame(width: 200, height: 200)
+        .background(.red)
+        
     }
 }
 
