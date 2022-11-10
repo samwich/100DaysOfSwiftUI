0e90b96aca6314f99bb95fe2898c9372368eaeef
Sam Johnson <sam@samwich.com>
2022-11-10 12:52:51 -0800

    basic structure

--- RockPaperScissors/RockPaperScissors/ContentView.swift
+++ RockPaperScissors/RockPaperScissors/ContentView.swift
@@ -8,12 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    let possibleMoves = ["Rock", "Paper", "Scissors"]
+    let maxTurns = 10
+    @State private var turnNumber = 0
+    @State private var score = 0
+    @State private var appMove = Int.random(in: 0...2)
+    @State private var turnGoal = Bool.random()
+
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Rock, Paper, Scissors")
+                .font(.largeTitle)
+            Spacer()
+            Text("Score: \(score)")
+                .font(.largeTitle)
+            Spacer()
+            Text("Opponent's move: \(possibleMoves[appMove])")
+                .font(.largeTitle)
+            Spacer()
+            Text("Try to: \(turnGoal ? "Win" : "Lose")")
+                .font(.largeTitle)
+            Spacer()
+            ForEach(possibleMoves, id: \.self) { move in
+                Button(move) {}
+                    .font(.largeTitle)
+                Spacer()
+            }
         }
         .padding()
     }
