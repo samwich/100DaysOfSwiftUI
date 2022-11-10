aa12bcf4d3616f9a31867e217b1ca886bf8363f6
Sam Johnson <sam@samwich.com>
2022-11-10 12:29:10 -0800

    Day 25 Rock Paper Scissors

---
+++ RockPaperScissors/RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Sam Johnson on 2022-11-10.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
