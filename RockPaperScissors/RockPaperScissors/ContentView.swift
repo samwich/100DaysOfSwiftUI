5c829a48f1193a8ae66d6e0526a2df55b621352c
Sam Johnson <sam@samwich.com>
2022-11-11 16:39:43 -0800

    Do some refactoring and make the interface worse(?)

--- RockPaperScissors/RockPaperScissors/ContentView.swift
+++ RockPaperScissors/RockPaperScissors/ContentView.swift
@@ -7,6 +7,20 @@
 
 import SwiftUI
 
+struct MoveLabel: View {
+    var text: String
+    
+    let stringToEmoji = [
+        "Rock": "🪨",
+        "Paper": "📜",
+        "Scissors": "✂️"
+    ]
+
+    var body: some View {
+        Text(text + (stringToEmoji[text] ?? ""))
+    }
+}
+
 struct ContentView: View {
     let possibleMoves = ["Rock", "Paper", "Scissors"]
     let winningMoves = ["Scissors", "Rock", "Paper"]
@@ -18,6 +32,51 @@ struct ContentView: View {
     @State private var turnGoal = Bool.random()
     @State private var showingSummary = false
     
+    var body: some View {
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [.orange, .red]), startPoint: .top, endPoint: .bottom)
+                .ignoresSafeArea()
+            VStack {
+                Text("Rock, Paper, Scissors")
+                    .font(.largeTitle)
+                Spacer()
+                Text("Score: \(score)")
+                    .font(.largeTitle)
+                Spacer()
+                Group {
+                    Text("Opponent's move:")
+                    MoveLabel(text: possibleMoves[appMove])
+                }
+                .font(.largeTitle)
+                Spacer()
+                Group {
+                    Text("Try to:")
+                    Text("\(turnGoal ? "Win 🙆" : "Lose 🙅")")
+                }
+                .font(.largeTitle)
+                Spacer()
+                ForEach(0..<3) { playerMove in
+                    Button {
+                        optionTapped(playerMove)
+                    } label: {
+                        MoveLabel(text: possibleMoves[playerMove])
+                    }
+                    .font(.largeTitle)
+                    .frame(width: 200, height: 75)
+                    .background(Color(.lightGray))
+                    .clipShape(Capsule())
+                }
+                Spacer()
+            }
+            .padding()
+        }
+        .alert("Game Over", isPresented: $showingSummary) {
+            Button("Play again", action: newGame)
+        } message: {
+            Text("Final score: \(score)")
+        }
+    }
+
     func newGame() {
         turnNumber = 1
         score = 0
@@ -25,51 +84,22 @@ struct ContentView: View {
         turnGoal = Bool.random()
     }
     
-    var body: some View {
-        VStack {
-            Text("Rock, Paper, Scissors")
-                .font(.largeTitle)
-            Spacer()
-            Text("Score: \(score)")
-                .font(.largeTitle)
-            Spacer()
-            Text("Opponent's move:")
-                .font(.largeTitle)
-            Text(possibleMoves[appMove])
-                .font(.largeTitle)
-            Spacer()
-            Text("Try to: \(turnGoal ? "Win" : "Lose")")
-                .font(.largeTitle)
-            Spacer()
-            ForEach(0..<3) { playerMove in
-                Button(possibleMoves[playerMove]) {
-                    
-                    if turnGoal && winningMoves[playerMove] == possibleMoves[appMove] {
-                        score += 1
-                    } else if !turnGoal && losingMoves[playerMove] == possibleMoves[appMove] {
-                        score += 1
-                    } else {
-                        score -= 1
-                    }
-                    
-                    if turnNumber < maxTurns {
-                        turnGoal.toggle()
-                        appMove = Int.random(in: 0...2)
-                        turnNumber += 1
-                    } else {
-                        showingSummary.toggle()
-                    }
-                }
-                .font(.largeTitle)
-                Spacer()
-            }
+    func optionTapped(_ playerMove: Int) {
+        if turnGoal && winningMoves[playerMove] == possibleMoves[appMove] {
+            score += 1
+        } else if !turnGoal && losingMoves[playerMove] == possibleMoves[appMove] {
+            score += 1
+        } else {
+            score -= 1
         }
-        .alert("Game Over", isPresented: $showingSummary) {
-            Button("Play again", action: newGame)
-        } message: {
-            Text("Final score: \(score)")
+        
+        if turnNumber < maxTurns {
+            turnGoal.toggle()
+            appMove = Int.random(in: 0...2)
+            turnNumber += 1
+        } else {
+            showingSummary.toggle()
         }
-        .padding()
     }
 }
 
