d21f465414de98c3dcf929e5becaec35ccabc5e9
Sam Johnson <sam@samwich.com>
2022-11-13 16:39:08 -0800

    Day 26 BetterRest

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Sam Johnson on 2022-11-13.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
