2b3f89ed94e9604da573c0a95740d8b3c3221fc1
Sam Johnson <sam@samwich.com>
2022-11-17 12:08:31 -0800

    Day 27 - Building a basic layout

--- BetterRest/BetterRest/ContentView.swift
+++ BetterRest/BetterRest/ContentView.swift
@@ -8,45 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var sleepAmount = 8.0
     @State private var wakeUp = Date.now
-    
+    @State private var sleepAmount = 8.0
+    @State private var coffeeAmount = 1
+
     var body: some View {
-        
-        VStack {
-            Stepper("\(sleepAmount.formatted())", value: $sleepAmount, in: 4...12, step: 0.25)
-            DatePicker("Please enter a date", selection: $wakeUp)
-                .labelsHidden()
-            DatePicker("Please enter a date", selection: $wakeUp, displayedComponents: .date)
-            DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
-            DatePicker("Please enter a future date", selection: $wakeUp, in: exampleDateRange())
-            DatePicker("Please enter a future date", selection: $wakeUp, in: Date.now...)
-            Text(exampleChooseWakeUpTime().formatted())
-            Text(Date.now, format: .dateTime.hour().minute())
-            Text(Date.now, format: .dateTime.year().month().day())
-            Text(Date.now.formatted(date: .long, time: .shortened))
+        NavigationView {
+            VStack {
+                Text("When do you want to wake up?")
+                    .font(.headline)
+                DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                    .labelsHidden()
+                Text("Desired amount of sleep")
+                    .font(.headline)
+                
+                Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+                
+                Text("Daily coffee intake")
+                    .font(.headline)
+                
+                Stepper(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups", value: $coffeeAmount, in: 1...20)
+            }
+            .navigationTitle("BetterRest")
+            .toolbar {
+                Button("Calculate", action: calculateBedtime)
+            }
         }
     }
     
-    func exampleDateRange() -> ClosedRange<Date> {
-        let tomorrow = Date.now.addingTimeInterval(86_400)
-        let range = Date.now...tomorrow
-        return range
+    func calculateBedtime() {
+        
     }
     
-    func exampleChooseWakeUpTime() -> Date {
-        var components = DateComponents()
-        components.hour = 8
-        components.minute = 0
-        let date = Calendar.current.date(from: components) ?? Date.now
-        return date
-    }
-
-    func exampleReadComponents() {
-        let components = Calendar.current.dateComponents([.hour, .minute], from: Date.now)
-        let hour = components.hour ?? 0
-        let minute = components.minute ?? 0
-    }
 }
 
 struct ContentView_Previews: PreviewProvider {
