053cfb6cf3b32967d857bbd0ac695d6e40f49010
Sam Johnson <sam@samwich.com>
2022-11-19 19:32:29 -0800

    Day 29 Word Scramble

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Sam Johnson on 2022-11-19.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
