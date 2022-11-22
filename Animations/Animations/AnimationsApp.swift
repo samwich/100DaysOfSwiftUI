b09408c108c50c23e007f2944c023c43d8d4bd67
Sam Johnson <sam@samwich.com>
2022-11-21 17:23:37 -0800

    Day 32 Animations

---
+++ Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Sam Johnson on 2022-11-21.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
