05ec5d38ea953d3c0c2082da3f75849fd3a65d05
Sam Johnson <sam@samwich.com>
2022-11-23 16:50:04 -0800

    Day 35 Challenge - TimesTables

---
+++ TimesTables/TimesTables/TimesTablesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimesTablesApp.swift
+//  TimesTables
+//
+//  Created by Sam Johnson on 2022-11-23.
+//
+
+import SwiftUI
+
+@main
+struct TimesTablesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
