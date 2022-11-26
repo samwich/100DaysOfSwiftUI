83bd3182e8680b3712a8bd771e177c242d98ac5e
Sam Johnson <sam@samwich.com>
2022-11-25 16:24:25 -0800

    Day 37 - begin iExpense implementation

--- iExpense/iExpense/ContentView.swift
+++ iExpense/iExpense/ContentView.swift
@@ -7,23 +7,10 @@
 
 import SwiftUI
 
-struct User: Codable {
-    let firstName: String
-    let lastName: String
-}
-
 struct ContentView: View {
-    @State private var user = User(firstName: "Taylor", lastName: "Swift")
-    
     var body: some View {
         VStack {
-            Button("Save User") {
-                let encoder = JSONEncoder()
-                
-                if let data = try? encoder.encode(user) {
-                    UserDefaults.standard.set(data, forKey: "UserData")
-                }
-            }
+            Text("hello")
         }
         .padding()
     }
---
+++ iExpense/iExpense/ExpenseItem.swift
@@ -0,0 +1,13 @@
+//
+//  ExpenseItem.swift
+//  iExpense
+//
+//  Created by Sam Johnson on 2022-11-25.
+//
+
+//import Foundation
+struct ExpenseItem {
+    let name: String
+    let type: String
+    let amount: Double
+}
