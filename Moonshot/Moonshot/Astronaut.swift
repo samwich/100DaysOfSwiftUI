cc39735f5d799bb0c296b7b49e7be6fa7b2a5e02
Sam Johnson <sam@samwich.com>
2022-11-28 14:43:09 -0800

    Day 40 - Loading a specific kind of Codable data

---
+++ Moonshot/Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Sam Johnson on 2022-11-28.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
--- Moonshot/Moonshot/ContentView.swift
+++ Moonshot/Moonshot/ContentView.swift
@@ -7,32 +7,11 @@
 
 import SwiftUI
 
-//struct User: Codable {
-//    let name: String
-//    let address: Address
-//}
-//
-//struct Address: Codable {
-//    let street: String
-//    let city: String
-//}
-
 struct ContentView: View {
-    let layout = [
-//        GridItem(.fixed(80)),
-//        GridItem(.fixed(80)),
-//        GridItem(.fixed(80)),
-        GridItem(.adaptive(minimum: 80, maximum: 120)),
-    ]
+    let astronauts = Bundle.main.decode("astronauts.json")
     
     var body: some View {
-        ScrollView {
-            LazyVGrid(columns: layout) {
-                ForEach(0..<1000) {
-                    Text("Item \($0)")
-                }
-            }
-        }
+        Text("\(astronauts.count)")
     }
 }
 
