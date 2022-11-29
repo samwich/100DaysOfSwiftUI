2fb115aa0a1b1125eb73c9fe4ec84702a3e99a32
Sam Johnson <sam@samwich.com>
2022-11-28 17:11:43 -0800

    Showing mission details with ScrollView and GeometryReader

--- Moonshot/Moonshot/ContentView.swift
+++ Moonshot/Moonshot/ContentView.swift
@@ -10,11 +10,11 @@ import SwiftUI
 struct ContentView: View {
     let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
     let missions: [Mission] = Bundle.main.decode("missions.json")
-
+    
     let columns = [
         GridItem(.adaptive(minimum: 150))
     ]
-
+    
     var body: some View {
         NavigationView {
             ScrollView {
