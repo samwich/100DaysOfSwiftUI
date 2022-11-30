5235c888fb3b950a2a772a258f921197af2ad33b
Sam Johnson <sam@samwich.com>
2022-11-30 15:54:31 -0800

    Day 43: Drawing

---
+++ Drawing/Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Sam Johnson on 2022-11-30.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
