3130b2af8f190d0c6bd90e2b86a0e54001cc5743
Sam Johnson <sam@samwich.com>
2022-11-30 16:04:57 -0800

    Creating custom paths with SwiftUI

--- Drawing/Drawing/ContentView.swift
+++ Drawing/Drawing/ContentView.swift
@@ -10,10 +10,16 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Path { path in
+                path.move(to: CGPoint(x: 200, y: 100))
+                path.addLine(to: CGPoint(x: 100, y: 300))
+                path.addLine(to: CGPoint(x: 300, y: 300))
+                path.addLine(to: CGPoint(x: 200, y: 100))
+//                path.closeSubpath()
+            }
+//            .fill(.blue)
+//            .stroke(.blue, lineWidth: 10)
+            .stroke(.blue, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
         }
         .padding()
     }
