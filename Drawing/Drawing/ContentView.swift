8bc27b1a5c86c91cb35053cfea58ac42bf9e8021
Sam Johnson <sam@samwich.com>
2022-12-05 13:49:24 -0800

    Creative borders and fills using ImagePaint

--- Drawing/Drawing/ContentView.swift
+++ Drawing/Drawing/ContentView.swift
@@ -7,42 +7,32 @@
 
 import SwiftUI
 
-struct Flower: Shape {
-    var petalOffset = -20.0
-    var petalWidth = 100.0
-    
-    func path(in rect: CGRect) -> Path {
-        var path = Path()
-        
-        for number in stride(from: 0, to: Double.pi * 2, by: Double.pi / 8) {
-            let rotation = CGAffineTransform(rotationAngle: number)
-            let position = rotation.concatenating(CGAffineTransform(translationX: rect.width / 2, y: rect.height / 2))
-            
-            let originalPetal = Path(ellipseIn: CGRect(x: petalOffset, y: 0, width: petalWidth, height: rect.width / 2))
-            let rotatedPetal = originalPetal.applying(position)
-            
-            path.addPath(rotatedPetal)
-        }
-        
-        return path
-    }
-}
-
 struct ContentView: View {
-    @State private var petalOffset = -20.0
-    @State private var petalWidth = 100.0
 
     var body: some View {
-        VStack {
-            Flower(petalOffset: petalOffset, petalWidth: petalWidth)
-                .fill(.red, style: FillStyle(eoFill: true))
-            Text("Offset")
-            Slider(value: $petalOffset, in: -40...40)
-                .padding([.horizontal, .bottom])
-            Text("Width")
-            Slider(value: $petalWidth, in: 0...100)
-                .padding([.horizontal, .bottom])
-        }
+//        Text("Hello World")
+//            .frame(width: 300, height: 300)
+//            .border(ImagePaint(image: Image("Example"), scale: 0.2), width: 50)
+//        Text("Hello World")
+//            .frame(width: 300, height: 300)
+//            .border(
+//                ImagePaint(
+//                    image: Image("Example"),
+//                    sourceRect: CGRect(x: 0.1, y: 0.15, width: 0.70, height: 0.5),
+//                    scale: 0.15
+//                ),
+//                width: 50
+//            )
+        Capsule()
+            .strokeBorder(
+                ImagePaint(
+                    image: Image("Example"),
+                    sourceRect: CGRect(x: 0.1, y: 0.15, width: 0.70, height: 0.5),
+                    scale: 0.15
+                ),
+                lineWidth: 30
+            )
+            .frame(width: 300, height: 200)
     }
 }
 
