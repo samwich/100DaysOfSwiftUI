72b4c4faaec7d0bd6937ffe9c7e4adf37a591e8d
Sam Johnson <sam@samwich.com>
2022-12-05 14:08:44 -0800

    Enabling high-performance Metal rendering with drawingGroup()

--- Drawing/Drawing/ContentView.swift
+++ Drawing/Drawing/ContentView.swift
@@ -7,32 +7,53 @@
 
 import SwiftUI
 
+struct ColorCyclingCircle: View {
+    var amount = 0.0
+    var steps = 100
+    
+    var body: some View {
+        ZStack {
+            ForEach(0..<steps) { value in
+                Circle()
+                    .inset(by: Double(value))
+//                    .strokeBorder(color(for: value, brightness: 1), lineWidth: 2)
+                    .strokeBorder(
+                        LinearGradient(
+                            colors: [
+                                color(for: value, brightness: 1),
+                                color(for: value, brightness: 0.3)
+                            ],
+                            startPoint: .top,
+                            endPoint: .bottom
+                        ),
+                        lineWidth: 2
+                    )
+            }
+        }
+        .drawingGroup()
+    }
+    
+    func color(for value: Int, brightness: Double) -> Color {
+        var targetHue = Double(value) / Double(steps) + amount
+        
+        if targetHue > 1 {
+            targetHue -= 1
+        }
+        
+        return Color(hue: targetHue, saturation: 1, brightness: brightness)
+    }
+}
+
 struct ContentView: View {
+    @State private var colorCycle = 0.0
 
     var body: some View {
-//        Text("Hello World")
-//            .frame(width: 300, height: 300)
-//            .border(ImagePaint(image: Image("Example"), scale: 0.2), width: 50)
-//        Text("Hello World")
-//            .frame(width: 300, height: 300)
-//            .border(
-//                ImagePaint(
-//                    image: Image("Example"),
-//                    sourceRect: CGRect(x: 0.1, y: 0.15, width: 0.70, height: 0.5),
-//                    scale: 0.15
-//                ),
-//                width: 50
-//            )
-        Capsule()
-            .strokeBorder(
-                ImagePaint(
-                    image: Image("Example"),
-                    sourceRect: CGRect(x: 0.1, y: 0.15, width: 0.70, height: 0.5),
-                    scale: 0.15
-                ),
-                lineWidth: 30
-            )
-            .frame(width: 300, height: 200)
+        VStack {
+            ColorCyclingCircle(amount: colorCycle)
+                .frame(width: 300, height: 300)
+            
+            Slider(value: $colorCycle)
+        }
     }
 }
 
