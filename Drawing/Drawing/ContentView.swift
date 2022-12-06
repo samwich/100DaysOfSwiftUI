9447058cf3fa22306e7cc296e2caeb87b3c9b950
Sam Johnson <sam@samwich.com>
2022-12-05 16:46:35 -0800

    Animating simple shapes with animatableData

--- Drawing/Drawing/ContentView.swift
+++ Drawing/Drawing/ContentView.swift
@@ -7,42 +7,38 @@
 
 import SwiftUI
 
+struct Trapezoid: Shape {
+    var insetAmount: Double
+    
+    var animatableData: Double {
+        get { insetAmount }
+        set { insetAmount = newValue }
+    }
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        path.move(to: CGPoint(x: 0, y: rect.maxY))
+        path.addLine(to: CGPoint(x: insetAmount, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX - insetAmount, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: 0, y: rect.maxY))
+
+        return path
+    }
+}
+
 struct ContentView: View {
-    @State private var amount = 0.0
+    @State private var insetAmount = 50.0
     
     var body: some View {
-        VStack {
-            ZStack {
-                Circle()
-                    .fill(.red)
-                    .frame(width: 200 * amount)
-                    .offset(x: -50, y: -80)
-                    .blendMode(.screen)
-                Circle()
-                    .fill(.green)
-                    .frame(width: 200 * amount)
-                    .offset(x: 50, y: -80)
-                    .blendMode(.screen)
-                Circle()
-                    .fill(.blue)
-                    .frame(width: 200 * amount)
-                    .blendMode(.screen)
+        Trapezoid(insetAmount: insetAmount)
+            .frame(width: 200, height: 200)
+            .onTapGesture {
+                withAnimation {
+                    insetAmount = Double.random(in: 10...90)
+                }
             }
-            .frame(width: 300, height: 300)
-
-            Image("Example")
-                .resizable()
-                .scaledToFit()
-                .frame(width: 300, height: 300)
-                .saturation(amount)
-                .blur(radius: (1 - amount) * 15)
-
-            Slider(value: $amount)
-                .padding()
-        }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background(.black)
-        .ignoresSafeArea()
     }
 }
 
