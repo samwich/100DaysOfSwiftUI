ee5b71624ca39c5f4821622c6ea25a4bc4c2a4f2
Sam Johnson <sam@samwich.com>
2022-12-10 18:00:25 -0800

    HabitTracking xcode project

---
+++ HabitTracker/HabitTracker/HabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerApp.swift
+//  HabitTracker
+//
+//  Created by Sam Johnson on 2022-12-10.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
