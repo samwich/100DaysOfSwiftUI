1e3f79b65bcd56fa27eb93aa497b0bb5bc460aa6
Sam Johnson <sam@samwich.com>
2022-12-14 16:50:07 -0800

    Day 53 - Bookworm

---
+++ Bookworm/Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Sam Johnson on 2022-12-14.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
