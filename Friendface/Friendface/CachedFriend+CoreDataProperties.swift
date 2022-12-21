88aaac98ff0019bd076c65cf22202114cfaf3f5f
Sam Johnson <sam@samwich.com>
2022-12-20 22:09:00 -0800

    regenerate CachedFriend properties for to-many relationship

--- Friendface/Friendface/CachedFriend+CoreDataProperties.swift
+++ Friendface/Friendface/CachedFriend+CoreDataProperties.swift
@@ -18,7 +18,24 @@ extension CachedFriend {
 
     @NSManaged public var id: UUID?
     @NSManaged public var name: String?
-    @NSManaged public var user: CachedUser?
+    @NSManaged public var user: NSSet?
+
+}
+
+// MARK: Generated accessors for user
+extension CachedFriend {
+
+    @objc(addUserObject:)
+    @NSManaged public func addToUser(_ value: CachedUser)
+
+    @objc(removeUserObject:)
+    @NSManaged public func removeFromUser(_ value: CachedUser)
+
+    @objc(addUser:)
+    @NSManaged public func addToUser(_ values: NSSet)
+
+    @objc(removeUser:)
+    @NSManaged public func removeFromUser(_ values: NSSet)
 
     public var wrappedName: String {
         name ?? "Unknown"
