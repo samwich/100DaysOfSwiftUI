91d64b855a4347f894e2c9e26c90fd57c5ea8985
Sam Johnson <sam@samwich.com>
2023-02-22 16:25:34 -0800

    Nomenclator locations in progress

--- Nomenclator/Nomenclator/Person.swift
+++ Nomenclator/Nomenclator/Person.swift
@@ -5,13 +5,31 @@
 //  Created by Sam Johnson on 2023-02-21.
 //
 
+import CoreLocation
 import Foundation
 
 struct Person: Codable, Equatable, Identifiable {
     let id: UUID
     var name: String
+    var latitude: Double
+    var longitude: Double
+    
+    var location: CLLocationCoordinate2D {
+        get { CLLocationCoordinate2D(latitude: latitude, longitude: longitude) }
+        set {
+            latitude = newValue.latitude
+            longitude = newValue.longitude
+        }
+    }
     
     var idString: String { id.uuidString }
+    
+    init(id: UUID, name: String, location: CLLocationCoordinate2D) {
+        self.id = id
+        self.name = name
+        self.latitude = location.latitude
+        self.longitude = location.longitude
+    }
     
     func imageURL() -> URL {
         return FileManager.documentsDirectory.appendingPathComponent(idString)
