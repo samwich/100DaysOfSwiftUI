4f23d6fadb66a4e0dac5b25aa24d5b2ca021435a
Sam Johnson <sam@samwich.com>
2023-09-22 17:40:00 -0700

    Filezilla: create project

---
+++ Flashzilla/Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Sam Johnson on 2023-09-22.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
